8cd18bf5a5753247e5ad80d7e88351e34e28ed14
nka21 <133028205+nka21@users.noreply.github.com>
2024-05-09 01:25:48 +0900

    たくさん追加

--- website/tailwind.config.mjs
+++ website/tailwind.config.mjs
@@ -1,14 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        // カラー設定
-        colors: {
-            // 背景色
-            base: '#FBEED9',
-            // 基本色
-            black: '#505050',
-        },
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      // フォントの設定
+      fontFamily: {
+        sans: [
+          'Inter',
+          'Helvetica Neue',
+          'Arial',
+          'Hiragino Kaku Gothic ProN',
+          'Hiragino Sans',
+          'Meiryo',
+          'sans-serif',
+        ],
+      },
     },
-    plugins: [],
+    // カラー設定
+    colors: {
+      // 背景色
+      base: '#FBEED9',
+      // 基本色
+      black: '#505050',
+      // テキストカラー1
+      gray: '#27374D',
+      // テキストカラー2
+      white: '#526D82',
+      // テキストカラー3
+      blue: '#5D6F89',
+      // ダークブルー
+      darkblue: '#445D6F',
+    },
+  },
+  plugins: [],
 };
